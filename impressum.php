99d6eba4d1d748fa6537326b25e89f7db8692850
windisch <windisch@sub.uni-goettingen.de>
2012-11-02 11:40:32 +0000

    ARCH-536 Migration Inhalt zu Konradi
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/ent/archaeo18/branches/design-konradi@585 aa2a3981-a392-0410-b963-7749bab6effd

--- impressum.php
+++ impressum.php
@@ -2,14 +2,34 @@
 <div class="content clearfix">
 	<div class="wrap">
 		<section>
-			<h2>Impressum</h2>
-			<p class="font-normal fixie"></p>
-			<p class="font-italic fixie"></p>
-			<p class="font-bold fixie"></p>
-			<h3 class="fixie"></h3>
-			<p class="font-normal fixie"></p>
-			<p class="font-italic fixie"></p>
-			<p class="font-bold fixie"></p>
+			<h2>Impressum / Anbieterhinweis</h2>
+			<p class="font-normal">Die nachstehenden Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben zur Anbieterkennzeichnung zu den datenschutzrechtlichen Informationspflichten sowie weitere wichtige rechtliche Hinweise zur Internetpräsenz der digitalen Edition „Christian Gottlob Heyne Vorlesungen über die Archäologie“.</p>
+			<h3>Anbieter</h3>
+			<p class="font-normal">Anbieter dieser Internetpräsenz ist im Rechtsinne die Niedersächsische Staats- und Universitätsbibliothek Göttingen.</p>
+			<h3>Adresse</h3>
+			<p class="font-normal">
+				Niedersächsische Staats- und Universitätsbibliothek Göttingen<br />
+				Georg-August-Universität Göttingen<br />
+				Platz der Göttinger Sieben 1<br />
+				37073 Göttingen<br />
+				Tel.: +49 (0)551 / 39-5212<br />
+				Fax: +49 (0)551 / 39-5222<br />
+				E-Mail: <a href="mailto:sekretariat@sub.uni-goettingen.de">sekretariat@sub.uni-goettingen.de</a><br />
+				Internet: <a href="http://www.sub.uni-goettingen.de" target="_blank" title="Website der Niedersächsischen Staats- und Universitätsbibltiothek Göttingen">www.sub.uni-goettingen.de</a><br />
+			</p>
+			<h3>Vertreter</h3>
+			<p class="font-normal">Die Niedersächsische Staats- und Universitätsbibliothek Göttingen (SUB) wird vertreten durch den Leitenden Direktor Prof. Dr. Norbert Lossau.<br />Die SUB Göttingen ist als zentrale Einrichtung eine organisatorische, rechtlich nicht selbständige Einheit der Georg-August-Universität Göttingen. Die Georg-August-Universität Göttingen ist eine Körperschaft des öffentlichen Rechts. Sie wird durch die Präsidentin Prof. Dr. Ulrike Beisiegel gesetzlich vertreten.</p>
+			<h3>Zuständige Aufsichtsbehörde</h3>
+			<p class="font-normal">Georg-August-Universität Göttingen Stiftung Öffentlichen Rechts, Stiftungsausschuss Universität, Wilhelmsplatz 1, 37073 Göttingen</p>
+			<h3>Umsatzsteueridentifikationsnummer</h3>
+			<p class="font-normal">Die Umsatzsteueridentifikationsnummer der SUB Göttingen lautet: DE 152 336 201 </p>
+			<h3>Haftung für Inhalte</h3>
+			<p class="font-normal">Die Niedersächsische Staats- und Universitätsbibliothek Göttingen (SUB Göttingen) ist als Diensteanbieterin für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Die SUB Göttingen ist um Richtigkeit und Aktualität der hier bereitgestellten Informationen bemüht. Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt, dennoch können Fehler und Unklarheiten nicht vollständig ausgeschlossen werden. Die SUB Göttingen übernimmt daher keine Gewähr für die Aktualität, Richtigkeit, Vollständigkeit oder Qualität der bereitgestellten Informationen.<br />Insbesondere sind wir als Diensteanbieter nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br />Für Schäden materieller oder immaterieller Art, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen unmittelbar oder mittelbar verursacht werden, haftet die SUB Göttingen nicht, sofern ihr nicht nachweislich vorsätzliches oder grob fahrlässiges Verschulden zur Last fällt. Gleiches gilt für kostenlos zum Download bereitgehaltene Software.<br />Die SUB Göttingen behält es sich vor, Teile des Internetangebots oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.</p>
+			<h3>Haftung für Links</h3>
+			<p class="font-normal">Unser Angebot enthält Verknüpfungen (Links) zu Webseiten von anderen Einrichtungen der Universität und zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die SUB Göttingen identifiziert sich deshalb ausdrücklich nicht mit dem Inhalt der verlinkten Seiten und macht sich die fremden Inhalte nicht zu Eigen.<br />Die gesetzten Links werden im wissenschaftlichen Sinne zitiert und zur Information für Studium, Forschung und Lehre zur Verfügung gestellt. Wir haben die verlinkten Seiten zum Zeitpunkt der erstmaligen Verknüpfung auf mögliche Rechtsverstöße überprüft. Inhalte, durch die eine mögliche zivil- oder strafrechtliche Verantwortlichkeit ausgelöst wird, waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.<br />Wenn die SUB Göttingen feststellt oder von anderen darauf hingewiesen wird, dass ein externes Angebot, auf das sie per Link verknüpft hat, eine zivil- oder strafrechtliche Verantwortlichkeit auslöst, wird sie diesen Link umgehend entfernen. Die SUB Göttingen distanziert sich ausdrücklich von derartigen Inhalten.</p>
+			<h3>Hinweise zum Urheber- und Kennzeichenrecht</h3>
+			<p class="font-normal">Die SUB Göttingen ist bestrebt, in allen Publikationen die Urheberrechte der verwendeten Grafiken, Tondokumente, Videosequenzen und Texte zu beachten, von ihr selbst erstellte Grafiken, Tondokumente, Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken, Tondokumente, Videosequenzen und Texte zurückzugreifen.<br />Alle innerhalb des Internetangebotes genannten und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen uneingeschränkt den Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass Markenzeichen nicht durch Rechte Dritter geschützt sind.<br />Das Layout der Homepage, die verwendeten Grafiken sowie die sonstigen Inhalte der Internetpräsenz der SUB Göttingen sind urheberrechtlich geschützt.<br />Jede vom Urheberrechtsgesetz nicht zugelassene Verwertung bedarf vorheriger ausdrücklicher Zustimmung der SUB Göttingen. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Fotokopien und Downloads von Web-Seiten für den privaten, wissenschaftlichen und nicht kommerziellen Gebrauch dürfen hergestellt werden.<br />Das Urheberrecht für das Universitätssiegel liegt ausdrücklich bei der Georg-August-Universität Göttingen.<br />Wir erlauben ausdrücklich und begrüßen das Zitieren unserer Dokumente und Webseiten sowie das Setzen von Links auf unsere Website.</p>
+			<p class="font-normal">&copy; SUB Göttingen. Alle Rechte vorbehalten.</p>
 		</section>
 	</div>
 </div>
--- start.php
+++ start.php
@@ -2,17 +2,25 @@
 <div class="content clearfix">
 	<div class="wrap">
 		<section>
+			<h2>Über das Projekt</h2>
+			<p class="font-normal">Von 1767 bis 1804 hielt Christian Gottlob Heyne an der Universität Göttingen in fast jedem Sommersemester eine Vorlesung mit dem Titel „Die Archäologie oder die Kenntniß der Kunst und der Kunstwerke des Alterthums“. Sie war die erste speziell der antiken Kunst gewidmete Lehrveranstaltung an einer Universität und fand regen Zuspruch. Mitschriften der Vorlesung zirkulierten in ganz Europa, doch Heyne selbst fand nie Gelegenheit, den von ihm erstmals systematisch aufbereiteten Stoff zu einem Handbuch auszuarbeiten. Umso größere Bedeutung kommt den z. T. sehr umfangreichen Mitschriften zu, von denen bisher 15 Exemplare bekannt geworden sind.</p>
+			<p class="font-normal">Auf dieser Webseite werden ## Mitschriften von Heynes Vorlesung in digitalisierter Form zugänglich gemacht, # davon auch in vollständiger Transkription. Hyperlinks in den Transkriptionen führen zu den einschlägigen Normdatenbanken für die im Text erwähnten Personen, Orte und Kunstwerke sowie zu digitalisierten Fassungen der von Heyne zitierten Publikationen und Abbildungen. Ziel dieser Online-Edition ist es, die herausragende Stellung von Heynes Lehrveranstaltung im Gefüge der archäologischen Wissensproduktion des 18. Jahrhunderts sichtbar und für weitere wissenschaftliche Studien zugänglich zu machen.</p>
+			<!--
 			<h2 class="fixie"></h2>
 			<p class="font-normal fixie"></p>
 			<p class="font-italic fixie"></p>
 			<p class="font-bold fixie"></p>
+			-->
 		</section>
 		<section class="contenttype-start textimg clearfix">
-			<h2 class="fixie"></h2>
+			<h2>Über Christian Gottlob Heyne</h2>
 			<div>
 				<img class="img" alt="" src="http://placehold.it/352x434" class="content-img-style1 fixie" />
 			</div>
 			<div>
+				<p class="font-normal">Christian Gottlob Heyne (1729–1812) war einer der bedeutendsten Gelehrten der Georg-August-Universität im 18. Jahrhundert. 1763 wurde er als Professor der Poesie und Beredsamkeit und Direktor des Philologischen Seminars nach Göttingen berufen. Bald übernahm er auch die Le	itung der Universitätsbibliothek und fungierte als Sekretär der Sozietät (heute Akademie) der Wissenschaften und als Herausgeber der „Göttingischen Anzeigen von gelehrten Sachen“.</p>
+				<p class="font-normal">Während seiner fast fünfzigjährigen Wirkungszeit als Hochschullehrer bestimmte er maßgeblich die Geschicke der Göttinger Universität. Durch seine großen Textausgaben zu Homer und Vergil erwarb er sich europaweites Ansehen als Vertreter einer umfassenden Altertumswissenschaft. Sein besonderes Interesse galt der ästhetischen Dimension der antiken Literatur und Kunst. Im Sinne J. J. Winckelmanns sah er im „Studium des schönen Alterthums“ ein Mittel zur Humanisierung der Gesellschaft.</p>
+				<!--
 				<p class="font-normal fixie"></p>
 				<p class="font-italic fixie"></p>
 				<p class="font-bold fixie"></p>
@@ -20,6 +28,7 @@
 				<p class="fixie"></p>
 				<p class="fixie"></p>
 				<p class="fixie"></p>
+				-->
 			</div>
 		</section>
 	</div>
