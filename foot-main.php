23971c7f07d942c25e15a180d7b3787755dbfc2c
mahnke <mahnke@sub.uni-goettingen.de>
2012-11-29 21:51:03 +0000

    changed location of js library
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/ent/archaeo18/branches/archaeo18@629 aa2a3981-a392-0410-b963-7749bab6effd

--- foot-main.php
+++ foot-main.php
@@ -32,7 +32,7 @@
 <script>window.jQuery || document.write('<script src="./js/libs/jquery-1.5.1.min.js"><\/script>')</script>
 
 
-<script src="./js/plugins.js"></script>
+<script src="./js/libs/plugins.js"></script>
 <script src="./js/libs/cookie.js"></script>
 <script src="./js/libs/fixie.js"></script>
 <script src="./js/libs/dataTables.js"></script>
