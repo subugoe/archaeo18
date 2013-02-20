b745e361a5fe4b57e4cd8cdcefdbf6eaef3164d9
jaenicke <stefan.jaenicke@gmx.de>
2013-02-20 17:46:20 +0000

    bookmarkable pages
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/ent/archaeo18/branches/archaeo18@730 aa2a3981-a392-0410-b963-7749bab6effd

--- foot-main.php
+++ foot-main.php
@@ -5,15 +5,15 @@
 		</div>
 		<div class="footer-col-bg-gradient clearfix">
 			<ul>
-				<li><a href="#" title="Lorem ipsum dolor sit amet" hreflang="de">Start</a></li>
-				<li><a href="#" title="Lorem ipsum dolor sit amet" hreflang="de">Handschriften</a></li>
-				<li><a href="#" title="Lorem ipsum dolor sit amet" hreflang="de">Indices</a></li>
-				<li><a href="#" title="Lorem ipsum dolor sit amet" hreflang="de">Edition</a></li>
+				<li><a href="javascript:void(0)" title="Lorem ipsum dolor sit amet" hreflang="de">Start</a></li>
+				<li><a href="javascript:void(0)" title="Lorem ipsum dolor sit amet" hreflang="de">Handschriften</a></li>
+				<li><a href="javascript:void(0)" title="Lorem ipsum dolor sit amet" hreflang="de">Indices</a></li>
+				<li><a href="javascript:void(0)" title="Lorem ipsum dolor sit amet" hreflang="de">Edition</a></li>
 			</ul>
 			<ul>
-				<li><a href="#" title="Lorem ipsum dolor sit amet" hreflang="de">Impressum</a></li>
-				<li><a href="#" title="Lorem ipsum dolor sit amet" hreflang="de">Nutzungsbedingungen</a></li>
-				<li><a href="#" id="linkhelp" title="Lorem ipsum dolor sit amet" hreflang="de">Hilfe</a></li>
+				<li><a href="javascript:void(0)" title="Lorem ipsum dolor sit amet" hreflang="de">Impressum</a></li>
+				<li><a href="javascript:void(0)" title="Lorem ipsum dolor sit amet" hreflang="de">Nutzungsbedingungen</a></li>
+				<li><a href="javascript:void(0)" id="linkhelp" title="Lorem ipsum dolor sit amet" hreflang="de">Hilfe</a></li>
 			</ul>
 		</div>
 		<div class="clearfix">
@@ -43,10 +43,8 @@
 <script>window.jQuery || document.write('<script src="./js/libs/jquery-1.5.1.min.js"><\/script>')</script>
 
 	<script>
-		var showDiv = function(div,link,e){
-			if( typeof e != "undefined" ){
-				e.preventDefault();
-			}
+
+		var showDiv = function(div,link){
 			$('#start_page').css('display','none');
 			$('#edition_page').css('display','none');
 			$('#indices_page').css('display','none');
@@ -56,37 +54,53 @@
 			$('#linkedition').removeClass('selected');
 			$('#linkindices').removeClass('selected');
 			$('#linkhandschriften').removeClass('selected');
+			if( typeof link != 'undefined' ){
+				$(link).addClass('selected');
+			}
 			$(div).css('display','block');
-			$(link).addClass('selected');
 		}
 
 		if( window.location.href.indexOf('?params') != -1 ){
 			showDiv('#edition_page','#linkedition');
 			EditionGui.gridLayout();
+		} else if( window.location.href.indexOf('?page=') != -1 ){
+			var data = window.location.href.split('?page=')[1];
+			var page = data, link;
+			if( data.indexOf('&link=') != -1 ){
+				var data2 = data.split('&link=');
+				page = data2[0];
+				link = data2[1];
+			}			
+			showDiv(page,link);
 		} else {
 			showDiv('#start_page','#linkstart');
 		}
 
-		$('#linkstart').click(function(e){
-			showDiv('#start_page','#linkstart',e);
+		$('#linkstart').click(function(){
+			showDiv('#start_page','#linkstart');
+			location.hash = "?page=#start_page&link=#linkstart";
 		});
 
-		$('#linkedition').click(function(e){
-			showDiv('#edition_page','#linkedition',e);
+		$('#linkedition').click(function(){
+			showDiv('#edition_page','#linkedition');
 			EditionGui.gridLayout();
+			location.hash = "?page=#edition_page&link=#linkedition";
 		});
 
-		$('#linkindices').click(function(e){
-			showDiv('#indices_page','#linkindices',e);
+		$('#linkindices').click(function(){
+			showDiv('#indices_page','#linkindices');
 			Indices.checkDisplay();
+			location.hash = "?page=#indices_page&link=#linkindices";
 		});
 
-		$('#linkhandschriften').click(function(e){
-			showDiv('#manuscripts_page','#linkhandschriften',e);
+		$('#linkhandschriften').click(function(){
+			showDiv('#manuscripts_page','#linkhandschriften');
+			location.hash = "?page=#manuscripts_page&link=#linkhandschriften";
 		});
 
 		$('#linkhelp').click(function(e){
 			showDiv('#help_page','#linkhelp',e);
+			location.hash = "?page=#help_page";
 		});
 
 	</script>
