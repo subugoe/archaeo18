89a097340e3fd74b6bf62542fb57134f8140307a
mahnke <mahnke@sub.uni-goettingen.de>
2013-02-21 13:33:45 +0000

    more local data
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/ent/archaeo18/branches/archaeo18@763 aa2a3981-a392-0410-b963-7749bab6effd

--- testdata/conf/EditionProperties.local.js
+++ testdata/conf/EditionProperties.local.js
@@ -111,13 +111,15 @@ var EditionProperties = {
 		maxTags: 50,		// maximum number of tags to be displayed in tag cloud
 		
 		// document related queries
-		documentEndpoint: 	'/exist/rest/db/archaeo18/queries/listDocs.xquery',
+		//documentEndpoint: 	'/exist/rest/db/archaeo18/queries/listDocs.xquery',
+		documentEndpoint: 	'./testdata/docs/listDocs.xquery',
 		textQuery: 		'/exist/rest/db/archaeo18/queries/getText.xq?mode=raw&format=xhtml&doc=DOC_ID&page=0',
 		pageQuery: 		'/exist/rest/db/archaeo18/queries/getText.xq?mode=raw&format=xhtml&doc=DOC_ID&page=PAGE_ID',
 		outlineQuery: 		'/exist/rest/db/archaeo18/queries/getText.xq?mode=structure&format=xhtml&doc=DOC_ID',
 		pageCountQuery: 	'/exist/rest/db/archaeo18/queries/pageCount.xquery?doc=DOC_ID',
 		searchQuery: 		'/exist/rest/db/archaeo18/queries/search.xq?query=QUERY_ID&mode=xhtml',
-		facetsQuery: 		'/exist/rest/db/archaeo18/queries/getFacets.xq',
+		//facetsQuery: 		'/exist/rest/db/archaeo18/queries/getFacets.xq',
+		facetsQuery: 		'./testdata/docs/getFacets.xq',
 		metadataQuery: 		'/exist/rest/db/archaeo18/queries/getText.xq?mode=header&format=xhtml&doc=DOC_ID',
 		//facetTableQuery:	'/exist/rest/db/archaeo18/queries/experimental/listEntities.xq?facet=FACET_ID&format=xhtml',
 		facetTableQuery:	'./testdata/indices/listEntities.xq?facet=FACET_ID&format=xhtml',
