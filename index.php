d79b206a4affe1aac9d4bf6ed89ae5bd6f09e1fc
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-10-01 18:17:34 +0200

    Fix integration of youtube video on indices page

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 		<div id="indicesSelection" class="wrap">
 		</div>
 		<div id="sectionContainer" class="wrap">
-		</div>
+			<br />
 			<section>
 				<iframe width="640" height="480" src="//www.youtube-nocookie.com/embed/XPHgane-r8s?rel=0" frameborder="0" allowfullscreen></iframe>
 			</section>
