e912ae0202ba968e98e2a9c0abdf8c9a7d4394d3
krvoigt <voigt@sub.uni-goettingen.de>
2013-10-29 17:16:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -106,7 +106,6 @@
 							<li><a href="#" id="help-suchen">Suchen</a></li>
 							<li><a href="#" id="help-arbeiten-mit-den-handschriften">Arbeiten mit den Mitschriften</a></li>
 							<li><a href="#" id="help-informationen-zu-personen-orten-etc">Informationen zu Personen Orten etc.</a></li>
-							<li><a href="#" id="help-aufrufen-externer-quellen">Aufrufen Externer Quellen</a></li>
 							<li><a href="#" id="help-verknuepfung-der-mappen">Verknüpfung der Mappen</a></li>
 							<li><a href="#" id="help-dfg-viewer-und-pdf-export">DFG Viewer und PDF Export</a></li>
 							<li><a href="#" id="help-automatische-ausrichtung">Automatische Ausrichtung</a></li>
