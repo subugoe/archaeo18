01695941e1af7f821603630d8cb095a8b23e44ed
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-12-02 15:03:48 +0100

    Fix for static html TEI file

--- Resources/Public/JavaScript/Archaeo18Config.js
+++ Resources/Public/JavaScript/Archaeo18Config.js
@@ -12,6 +12,6 @@
 		"mapQuery":		"./testdata/kml/listEntities.xq?facet=FACET_ID&format=kml",
 		"tagcloudPageQuery":	"/exist/rest/db/archaeo18/queries/getText.xq?doc=DOC_ID&page=PAGE_ID&format=cloud&facet=FACET_ID",
 		"kmlQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?format=kml&doc=DOC_ID&page=PAGE_ID",
-		"teiUri": 		"/exist/rest/db/archaeo18/data/tei/DOC_ID.xml",
+		"teiUri": 		"content/tei/DOC_ID.xml",
 		"metsUri": 		"/exist/rest/db/archaeo18/data/mets/DOC_ID.mets.xml"
 }
