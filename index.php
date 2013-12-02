dcae01bd123cf345ada1b791d9ce5345a40286cd
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-12-02 12:07:13 +0100

    Fixed manuscripts pages

--- index.php
+++ index.php
@@ -56,19 +56,11 @@
 										<a href="#">Allgemeine Informationen</a><span class="icon-youtube-play"></span>
 									</li>
 
-									<li id="berlin-ms-germ-qrt-1666" class="scriptList">
-										<a href="#">Vorlesungsmitschrift 1775 (Berlin)</a>
-									</li>
-
-									<li id="rom-heyne1798" class="scriptList"><a href="#">
-											Vorlesungsmitschrift 1798 (Rom)</a></li>
-
-									<li id="weimar-hs-2056" class="scriptList"><a href="#">
-											Vorlesungsmitschrift 1772 (Weimar)</a></li>
-
-									<li id="bern-mss-muel-507" class="scriptList"><a href="#">
-											Vorlesungsmitschrift 1780 (Bern)</a></li>
-
+									<li id="berlin-ms-germ-qrt-1666" class="scriptList"><a href="#">Vorlesungsmitschrift 1775 (Berlin)</a></li>
+									<li id="rom-heyne1798" class="scriptList"><a href="#">Vorlesungsmitschrift 1798 (Rom)</a></li>
+									<li id="weimar-hs-2056" class="scriptList"><a href="#">Vorlesungsmitschrift 1772 (Weimar)</a></li>
+									<li id="bern-mss-muel-507" class="scriptList"><a href="#">Vorlesungsmitschrift 1780 (Bern)</a></li>
+									<li id="bern_1785" class="scriptList"><a href="#">Vorlesungsmitschrift 1785 (Bern)</a></li>
 								</ul>
 							</div>
 
