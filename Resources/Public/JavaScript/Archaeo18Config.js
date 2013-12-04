49a41c8fd832946777bd237080f5906887a21a0c
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-12-04 08:58:48 +0100

    Static files for outline

--- Resources/Public/JavaScript/Archaeo18Config.js
+++ Resources/Public/JavaScript/Archaeo18Config.js
@@ -3,7 +3,7 @@
 		"documentEndpoint": 	"./testdata/docs/listDocs.xquery",
 		"textQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?mode=raw&format=xhtml&doc=DOC_ID&page=0",
 		"pageQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?mode=raw&format=xhtml&doc=DOC_ID&page=PAGE_ID",
-		"outlineQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?mode=structure&format=xhtml&doc=DOC_ID",
+		"outlineQuery": 		"content/app/structure/DOC_ID.html",
 		"searchQuery": 		"/exist/rest/db/archaeo18/queries/search.xq?query=QUERY_ID&mode=xhtml",
 		"facetsQuery": 		"./testdata/docs/getFacets.xq",
 		"metadataQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?mode=header&format=xhtml&doc=DOC_ID",
@@ -12,6 +12,6 @@
 		"mapQuery":		"./testdata/kml/listEntities.xq?facet=FACET_ID&format=kml",
 		"tagcloudPageQuery":	"/exist/rest/db/archaeo18/queries/getText.xq?doc=DOC_ID&page=PAGE_ID&format=cloud&facet=FACET_ID",
 		"kmlQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?format=kml&doc=DOC_ID&page=PAGE_ID",
-		"teiUri": 		"content/tei/DOC_ID.xml",
+		"teiUri": 		"content/app/tei/DOC_ID.xml",
 		"metsUri": 		"/exist/rest/db/archaeo18/data/mets/DOC_ID.mets.xml"
 }
