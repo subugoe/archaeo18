94e1dc929a485aee9f21d2b21b7cd8ecf5949b35
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-01-17 16:13:35 +0100

    Fixed vagrant configuration

--- cookbooks/archaeo18/recipes/default.rb
+++ cookbooks/archaeo18/recipes/default.rb
@@ -1,17 +1,13 @@
 execute "apt-get update"
 
-# http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
-execute 'DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade'
-execute 'DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade'
-
-packages = ["openjdk-7-jdk", "ant", "libjaxp1.3", "htop", "git"]
+packages = ["openjdk-7-jdk", "ant", "libjaxp1.3", "htop", "git", "vim"]
 
 packages.each do |p|
 	package p
 end
 
 # make a small shell script to start archaeo18
-cookbook_file "/usr/local/bin/archaeo18" do
+cookbook_file "/usr/bin/archaeo18" do
 	source "archaeo18"
 	mode "0755"
 end
@@ -22,4 +18,5 @@ cookbook_file "/etc/init/archaeo18.conf" do
 	mode "0644"
 end
 
-execute "cd /home/vagrant/archaeo18/ && ant serve > /dev/null 2>&1 &"
+execute "initctl reload-configuration"
+execute "start archaeo18"
