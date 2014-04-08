8195fa3a09f7518b1620825ad340c585ef5af062
dsusk <tech@sub.uni-goettingen.de>
2014-04-08 12:33:21 +0200

    Raised submodule pointer

--- Resources/Public/JavaScript/Archaeo18Config.js
+++ Resources/Public/JavaScript/Archaeo18Config.js
@@ -5,16 +5,16 @@
 		"pageQuery": 		"content/app/xhtml-pages/DOC_ID-PAGE_ID.xhtml",
 		"outlineQuery": 		"content/app/structure/DOC_ID.xml",
 		"searchQuery": 		"/solr/archaeo18/select?q=content%3AQUERY_ID&wt=xslt&indent=true&hl=true&hl.fl=content&hl.fragsize=0&tr=solrresponse.xsl",
-		"facetsQuery": 		"content/app/facets.xml",
+		"facetsQuery": 		"/exist/rest/db/archaeo18/queries/getFacets.xq",
 		"metadataQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?mode=header&format=xhtml&doc=DOC_ID",
-		"facetTableQuery":	"content/app/indices/FACET_ID.xhtml",
-		"tagcloudQuery":		"content/app/cloud/FACET_ID.xhtml",
-		"mapQuery":		"./testdata/kml/listEntities.xq?facet=FACET_ID&format=kml",
+		"facetTableQuery":	"/exist/rest/db/archaeo18/queries/experimental/listEntities.xq?facet=FACET_ID&format=xhtml",
+		"facetTableQuery":	"./testdata/indices/listEntities.xq?facet=FACET_ID&format=xhtml",
+		"tagcloudQuery":		"/exist/rest/db/archaeo18/queries/experimental/listEntities.xq?facet=FACET_ID&format=cloud",
+		"mapQuery":		"/exist/rest/db/archaeo18/queries/experimental/listEntities.xq?facet=FACET_ID&format=kml",
 		"tagcloudPageQuery":	"/exist/rest/db/archaeo18/queries/getText.xq?doc=DOC_ID&page=PAGE_ID&format=cloud&facet=FACET_ID",
 		"kmlQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?format=kml&doc=DOC_ID&page=PAGE_ID",
 		"teiUri": 		"content/app/tei/DOC_ID.xml",
-		"metsUri": 		"content/app/mets/DOC_ID.xml",
+		"metsUri": 		"content/app/mets/DOC_ID.mets.xml",
 		"openDocumentsOnStart": true,
 		"documentOnStart": "berlin-ms-germ-qrt-1666"
 }
-
