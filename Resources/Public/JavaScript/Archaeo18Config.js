09a712424b2ad94aaf75ba8e06c5cbde7a8ab8e0
dsusk <tech@sub.uni-goettingen.de>
2014-07-09 10:11:47 +0200

    pdflink, kml-pages, tagcloud from file system

--- Resources/Public/JavaScript/Archaeo18Config.js
+++ Resources/Public/JavaScript/Archaeo18Config.js
@@ -6,14 +6,14 @@
 		"outlineQuery": 		"content/app/structure/DOC_ID.xml",
 		"searchQuery": 		"/solr/archaeo18/select?q=content%3AQUERY_ID&wt=xslt&indent=true&hl=true&hl.fl=content&hl.fragsize=0&tr=solrresponse.xsl",
 		"facetsQuery": 		"content/app/facets.xml",
-		"metadataQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?mode=header&format=xhtml&doc=DOC_ID",
 		"facetTableQuery":	"content/app/indices/FACET_ID.xhtml",
 		"tagcloudQuery":		"content/app/cloud/FACET_ID.xhtml",
 		"mapQuery":		"./testdata/kml/listEntities.xq?facet=FACET_ID&format=kml",
-		"tagcloudPageQuery":	"/exist/rest/db/archaeo18/queries/getText.xq?doc=DOC_ID&page=PAGE_ID&format=cloud&facet=FACET_ID",
-		"kmlQuery": 		"/exist/rest/db/archaeo18/queries/getText.xq?format=kml&doc=DOC_ID&page=PAGE_ID",
+		"tagcloudPageQuery":	"content/app/cloud/DOC_ID-PAGE_ID-FACET_ID.xml",
+                "kmlQuery":	"content/app/kml-pages/DOC_ID-PAGE_ID.kml",
 		"teiUri": 		"content/app/tei/DOC_ID.xml",
 		"metsUri": 		"content/app/mets/DOC_ID.xml",
+                "pdfLink":	"content/app/pdf/DOC_ID-800.pdf",
 		"openDocumentsOnStart": true,
 		"documentOnStart": "berlin-ms-germ-qrt-1666"
 }
